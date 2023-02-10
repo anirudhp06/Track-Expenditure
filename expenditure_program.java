c35563565f76bb9e2d81177738a840d30be24724
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-02-10 19:47:27 +0530

    Created window with greetings

--- expenditure_program.java
+++ expenditure_program.java
@@ -1,5 +1,31 @@
+import java.sql.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.*;
 public class expenditure_program{
     public static void main(String[] args){
-        System.out.println("Initial Commit");
+        try{
+            Connection con;
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            String url="jdbc:oracle:thin:@localhost:1521:XE";
+            String username="project",password="project";
+            con=DriverManager.getConnection(url,username,password);
+            System.out.println("connected");
+            JFrame f1=new JFrame();
+            JLabel l1=new JLabel("Hello welcome to expenditure tracking program");
+            f1.setLayout(null);
+            f1.setTitle("Expenditure System");
+            f1.setBounds(100,0,1280,720);
+            f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            l1.setHorizontalAlignment(JLabel.CENTER);
+            l1.setSize(1000,100);
+            f1.add(l1);
+            f1.setVisible(true);
+            con.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
